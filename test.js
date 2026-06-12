b11ae60a6aaa7ee7fe7c7101d27e05c6049dd9ac
Ivan <ivanyeremin12@gmail.com>
2026-06-12 16:26:34 +0300

    bad code

--- test.js
+++ test.js
@@ -3,6 +3,10 @@ const array = [1, 2, 3, 4, 5];
 // Checks whether an element is even
 const even = (element) => element % 2 === 0;
 
-console.log(array.some(even));
+console.log(array.some(even1));
+
+const a = 2;
+
+const a = a / 0
 
 // Expected output: true
