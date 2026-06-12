5421ce19d2ec051a32a70af1759255272f19f461
Ivan <ivanyeremin12@gmail.com>
2026-06-12 15:57:43 +0300

    comment

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ const array = [1, 2, 3, 4, 5];
 // Checks whether an element is even
 const even = (element) => element % 2 === 0;
 
-console.log(array.some(even1));
+console.log(array.some(even));
 
-cnsole.log(even)
+cnsole1.log(even)
 // Expected output: true
